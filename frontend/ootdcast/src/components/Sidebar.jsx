27b919dd2ed343cf291d73d35623a5be24c2040b
ShammirJ <shammir@ualberta.ca>
2025-02-15 18:04:11 -0700

    sidebar component

---
+++ frontend/ootdcast/src/components/Sidebar.jsx
@@ -0,0 +1,20 @@
+import Link from 'next/link';
+import styles from './Sidebar.module.css';
+
+export default function Sidebar() {
+    return (
+      <nav className={styles.sidebar}>
+        <ul>
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/library">Library</Link>
+          </li>
+          <li>
+            <Link href="/history">History</Link>
+          </li>
+        </ul>
+      </nav>
+    );
+  }
