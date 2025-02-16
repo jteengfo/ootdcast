4112ef5c94e4063e2fad37a0511b800cccea5abf
ShammirJ <shammir@ualberta.ca>
2025-02-16 11:47:53 -0700

    date component implemented

--- frontend/ootdcast/src/components/Greeting.jsx
+++ frontend/ootdcast/src/components/Greeting.jsx
@@ -1,10 +1,16 @@
 import styles from './Greeting.module.css';
 import Hello from './Hello';
+import DateComponent from './DateComponent'
 
 export default function Greeting() {
     return (
-        <div className={styles.hello}>
-            <Hello />
+        <div className={styles.greetingCard}>
+            <div className={styles.hello}>
+                <Hello />
+            </div>
+            <div className={styles.dateComponent}>
+                <DateComponent />
+            </div>
         </div>
     );
 }
