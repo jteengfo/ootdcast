d4c2ade04baf1e05be10369bb2f213009aaae9ed
Arshit Jain <arshit@Arshits-MacBook-Air-4.local>
2025-02-15 19:58:52 -0700

    Update

--- backend/ootdcast/ootdcastapp/urls.py
+++ backend/ootdcast/ootdcastapp/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('users/', views.UserList.as_view()),
     path('users/create/', views.UserCreate.as_view()),
-    path('clothing/delete/<int:pk>/', views.ClothingItemDelete.as_view(), name='delete-clothing')
+    path('clothing/delete/<int:pk>/', views.ClothingItemDelete.as_view(), name='delete-clothing'),
+    path('weather/', views.get_weather, name='get-weather'),
 ]
--- backend/ootdcast/ootdcastapp/views.py
+++ backend/ootdcast/ootdcastapp/views.py
@@ -3,6 +3,9 @@ from django.shortcuts import render
 from rest_framework import generics
 from .models import User, ClothingItem, Outfit
 from .serializers import UserSerializer, ClothingItemSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .weather import get_weather_data
 
 
 # Create your views here.
@@ -17,3 +20,12 @@ class UserCreate(generics.CreateAPIView):
 class ClothingItemDelete(generics.DestroyAPIView):
     queryset = ClothingItem.objects.all()
     serializer_class = ClothingItemSerializer
+
+@api_view(['GET'])
+def get_weather(request):
+    city = request.query_params.get('city', 'Edmonton')
+    weather_data = get_weather_data(city)
+    
+    if weather_data:
+        return Response(weather_data)
+    return Response({'error': 'Failed to fetch weather data'}, status=400)
