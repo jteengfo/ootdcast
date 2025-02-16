78061bb669df1438e81cae382da13d3c8749154c
ShammirJ <shammir@ualberta.ca>
2025-02-16 12:12:51 -0700

    save progress

---
+++ frontend/ootdcast/src/components/PreviousOutfit.jsx
@@ -0,0 +1,9 @@
+import styles from './PreviousOutfit.module.css'
+
+export default function PreviousOutfit() {
+    return(
+        <div className={styles.previousOutfitCard}>
+            
+        </div>
+    );
+}
