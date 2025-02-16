1c18a218696c05735656ad0d6505bb63857e2480
ShammirJ <shammir@ualberta.ca>
2025-02-16 12:48:05 -0700

    linking the pages

---
+++ frontend/ootdcast/src/app/library/page.js
@@ -0,0 +1,23 @@
+import styles from "./page.module.css";
+import Header from "../../components/Header";
+import Sidebar from "../../components/Sidebar";
+//import Greeting from "./../components/Greeting";
+import SearchBar from "../../components/Searchbar";
+
+export default function Library(){
+    return(
+        <div className={styles.page}>
+            <div className={styles.header}>
+                <SearchBar />
+                <Header />
+            </div>
+            <div className={styles.content}>
+                <div className={styles.sidebar}>
+                    <Sidebar />
+                </div>
+                <main className={styles.main}>
+                </main>
+            </div>
+        </div>
+    );
+}  
