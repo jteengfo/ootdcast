3f95f9c4e6fb7fd1970c141c66dde3a7716da09a
jteengfo <jamesang@ualberta.ca>
2025-02-16 20:28:23 -0700

    implemented logout

--- backend/ootdcast/ootdcastapp/urls.py
+++ backend/ootdcast/ootdcastapp/urls.py
@@ -4,7 +4,12 @@ from . import views
 urlpatterns = [
     path('users/', views.UserList.as_view()),
     path('users/create/', views.create_user, name='create_user'),
+
+    # auth
     path('auth/login/', views.user_login, name='user_login'),
+    path('auth/logout/', views.user_logout, name='user_logout'),
+
+    # weather
     path('weather/', views.get_weather, name='get-weather'),
 
     # clothing
